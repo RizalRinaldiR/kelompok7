0e93b9d0184dc52b455788e28165c66290874132
RizalRinaldiR <rizalramadhan533@gmail.com>
2024-10-27 16:22:58 +0700

    menambahkan halaman register

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Tambahkan path yang sesuai dengan struktur folder
+    "./pages/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
